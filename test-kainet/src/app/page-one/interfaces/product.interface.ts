bfab79d9790fdcab30c138146a396a9d96037753
Manuel Lopez <manu1475963@gmail.com>
2024-05-17 16:31:37 -0300

    feat: :alembic: Add table

---
+++ test-kainet/src/app/page-one/interfaces/product.interface.ts
@@ -0,0 +1,7 @@
+export interface Product {
+    companyName: string;
+    productName: string;
+    availabilityDate: string;
+    price: number;
+    currency: string;
+}
--- test-kainet/src/app/page-one/page-one.component.ts
+++ test-kainet/src/app/page-one/page-one.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { Product } from './interfaces/product.interface';
 
 @Component({
-  selector: 'app-page-one',
-  standalone: true,
-  imports: [],
-  templateUrl: './page-one.component.html',
-  styleUrl: './page-one.component.scss'
+    selector: 'app-page-one',
+    standalone: true,
+    imports: [],
+    templateUrl: './page-one.component.html',
+    styleUrl: './page-one.component.scss'
 })
 export class PageOneComponent {
+    products: Product[] = [
+        { companyName: 'Company A', productName: 'Product 1', availabilityDate: '2024-06-01', price: 100, currency: 'USD' },
+        { companyName: 'Company B', productName: 'Product 2', availabilityDate: '2024-07-15', price: 200, currency: 'EUR' },
+        // Add more products as needed
+    ];
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
 
 }
